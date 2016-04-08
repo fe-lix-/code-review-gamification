8da48b6cc778dda57e1a4b8d959302c9d5ad1c8d
Felix Delval <fdelval@ebay.com>
2016-04-08 14:46:31 +0200

    Link to badge page

--- src/BadgeBundle/Controller/DefaultController.php
+++ src/BadgeBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace BadgeBundle\Controller;
 use BadgeBundle\Entity\Badge;
 use BadgeBundle\Entity\BadgeAwarded;
 use EventBundle\Entity\RepositoryEvent;
+use GamificationBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,10 +13,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/badges/{user}")
+     * @Route("/badges/{username}")
      */
-    public function indexAction($user)
+    public function indexAction($username)
     {
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['name' => $username]);
+
         $badges = $this->container->get('badge_bundle.doctrine.orm.badge_awarded_repository')
             ->findAll();
 
@@ -28,13 +31,13 @@ class DefaultController extends Controller
         $awarded[] = new BadgeAwarded(new Badge('cr500', '500 code reviews'));
         $awarded[] = new BadgeAwarded(new Badge('cr1000', '1000 code reviews'));
 
-        $counters = $this->container->get('event_bundle.doctrine.orm.counter_repository')
+        $counters = $this->container->get('gamification_bundle.doctrine.orm.counter_repository')
             ->getCounterFor($user);
 
         return $this->render(
             'BadgeBundle:Default:index.html.twig',
             [
-                'username' => $user,
+                'user' => $user,
                 'badges' => $awarded,
                 'counters' => $counters,
             ]
